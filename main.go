08c2451be2bfda4252e4436f1d70c0cb1b2819d0
Thanh Lee <thanh.le-viet@quadram.ac.uk>
2021-06-25 05:06:01 +0100

    Update main.go

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "Welcome to CI/CD Pipeline lab - Thanh Lee"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
